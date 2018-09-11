60589a83745f09ccee5824d6d5eb15008eaf89f6
Arnaud Renaud <arnaud.renaud@gmail.com>
2018-09-11 09:40:07 +0200

    Part 3 & 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
-import { Instructions } from './components'
+import { Counter, Instructions } from './components'
 import './styles/app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initialCount: 0,
+      isValid: true
+    }
+
+    this.setInitialCount = this.setInitialCount.bind(this)
+  }
+
+  setInitialCount(event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    const initialCount = parseInt(data.get('initialCount'), 10)
+    const isValid = Number.isInteger(initialCount)
+    this.setState({
+      initialCount,
+      isValid
+    })
+  }
+
   render() {
     return (
       <div className="app">
@@ -11,6 +32,12 @@ class App extends Component {
           items={['lol', 'funny', 'ha']}
           shouldDisplayImage={false}
         />
+        <form onSubmit={this.setInitialCount}>
+          {!this.state.isValid ? <label>Please enter a number</label> : null}
+          <input name="initialCount" type="text" />
+          <input type="submit" />
+        </form>
+        <Counter count={this.state.initialCount} />
       </div>
     )
   }
---
+++ src/components/Counter.component.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: this.props.count
+    }
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({ count: props.count })
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.count}
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    )
+  }
+}
+
+export default Counter
